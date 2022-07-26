375a74b55d778aa03503da4c90eb58327062194c
Khushi Mehta <khushimehta8282@gmail.com>
2022-07-26 21:27:28 +0530

    Improved hotel schema

--- models/Hotel.js
+++ models/Hotel.js
@@ -49,6 +49,192 @@ const HotelSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    property: {
+        freeParking: {
+            type: Boolean,
+            default: false,
+        },
+        valetParking: {
+            type: Boolean,
+            default: false,
+        },
+        freeWiFi: {
+            type: Boolean,
+            default: false,
+        },
+        pool: {
+            type: Boolean,
+            default: false,
+        },
+        outdoorPool: {
+            type: Boolean,
+            default: false,
+        },
+        gym: {
+            type: Boolean,
+            default: false,
+        },
+        bar: {
+            type: Boolean,
+            default: false,
+        },
+        freeKidsStay: {
+            type: Boolean,
+            default: false,
+        },
+        airTransportation: {
+            type: Boolean,
+            default: false,
+        },
+        conference: {
+            type: Boolean,
+            default: false,
+        },
+        coffeeShop: {
+            type: Boolean,
+            default: false,
+        },
+        freeBreakfast: {
+            type: Boolean,
+            default: false,
+        },
+        welcomeDrink: {
+            type: Boolean,
+            default: false,
+        },
+        carHire: {
+            type: Boolean,
+            default: false,
+        },
+        taxiService: {
+            type: Boolean,
+            default: false,
+        },
+        banquetRoom: {
+            type: Boolean,
+            default: false,
+        },
+        meetingRoom: {
+            type: Boolean,
+            default: false,
+        },
+        security: {
+            type: Boolean,
+            default: false,
+        },
+        newspaper: {
+            type: Boolean,
+            default: false,
+        },
+        firstaid: {
+            type: Boolean,
+            default: false,
+        },
+        dryCleaning: {
+            type: Boolean,
+            default: false,
+        },
+        ironing: {
+            type: Boolean,
+            default: false,
+        },
+        laundry: {
+            type: Boolean,
+            default: false,
+        },
+        frontDesk24h: {
+            type: Boolean,
+            default: false,
+        },
+        currencyExchange: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    room: {
+        blackoutCurtains: {
+            type: Boolean,
+            default: false,
+        },
+        soundproof: {
+            type: Boolean,
+            default: false,
+        },
+        airConditioning: {
+            type: Boolean,
+            default: false,
+        },
+        desk: {
+            type: Boolean,
+            default: false,
+        },
+        diningArea: {
+            type: Boolean,
+            default: false,
+        },
+        tv: {
+            type: Boolean,
+            default: false,
+        },
+        shower: {
+            type: Boolean,
+            default: false,
+        },
+        bathrobes: {
+            type: Boolean,
+            default: false,
+        },
+        houseKeeping: {
+            type: Boolean,
+            default: false,
+        },
+        roomService: {
+            type: Boolean,
+            default: false,
+        },
+        seatingArea: {
+            type: Boolean,
+            default: false,
+        },
+        sofa: {
+            type: Boolean,
+            default: false,
+        },
+        telephone: {
+            type: Boolean,
+            default: false,
+        },
+        wardrobe: {
+            type: Boolean,
+            default: false,
+        },
+        clothesRack: {
+            type: Boolean,
+            default: false,
+        },
+        complimentaryToiletries: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    roomTypes: {
+        landmarkView: {
+            type: Boolean,
+            default: false,
+        },
+        suites: {
+            type: Boolean,
+            default: false,
+        },
+        noSmoking: {
+            type: Boolean,
+            default: false,
+        },
+        cityView: {
+            type: Boolean,
+            default: false,
+        },
+    }
 })
 
 export default mongoose.model("Hotel", HotelSchema)
